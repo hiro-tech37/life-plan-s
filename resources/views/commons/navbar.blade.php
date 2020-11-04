279ff6544d96994c65b00439b8db2cdf30b0f52a
hiroki.ishii <hiro.tech37@gmail.com>
2020-11-04 21:29:36 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/">家計サイズの保険サイズ</a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- リンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">link-A</a></li>
+                {{-- リンク --}}
+                <li class="nav-item"><a href="#" class="nav-link">link-B</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
